498f2715630d29f71787d48810e01c383c847534
879spiker <9mzx898785@gmail.com>
2023-10-04 21:20:35 +0800

    add initdata

--- src/main/resources/DB/data.sql
+++ src/main/resources/DB/data.sql
@@ -1,4 +1,10 @@
-INSERT INTO vote_record (voter_name, vote_item_id) VALUES
+-- 插入投票項目資料
+INSERT INTO vote_items (name) VALUES
+    ('電腦'),
+    ('滑鼠');
+
+-- 插入投票紀錄資料
+INSERT INTO vote_records (voter, vote_item_id) VALUES
     ('Leo', 1),
     ('Sandy', 1),
     ('Sandy', 2),
